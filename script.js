918ec0567d11ef469ba13c6be0da619083c8c5e6
Stelladelmar <113214552+Stelladelmar@users.noreply.github.com>
2022-09-17 14:24:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 const projectName = 'tribute-page';
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('text').value)));
+
+});
