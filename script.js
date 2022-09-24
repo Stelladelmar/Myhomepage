0ac9990c38738a01657ba143b6a8adb0aa1200ae
Stelladelmar <113214552+Stelladelmar@users.noreply.github.com>
2022-09-24 12:23:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,10 @@ var click_count;
 click_count = 0;
 
 
-document.getElementById('button').addEventListener('click', (event) => {
+document.getElementById('button1').addEventListener('click', (event) => {
   click_count = (typeof click_count === 'number' ? click_count : 0) + 1;
   event.target.innerText = click_count;
+
+});
 
 });
